e7d9eb9689a22ae8c750f7489687b8a442510afa
sivaraman <sivaraman.p@fr8.in>
2024-03-01 14:04:12 +0530

    version 4

--- src/update.tsx
+++ src/update.tsx
@@ -9,18 +9,22 @@ const InitializeApp= ()=> {
       App.addListener('appStateChange', async(state) => {
         if (state.isActive) {
           // Do the download during user active app time to prevent failed download
-          try{
           data = await CapacitorUpdater.download({
-          version: '0.0.3',
+          version: '0.0.4',
           url: 'https://github.com/Sivaraman1530/newAutoUpdate/archive/refs/heads/master.zip',
           })
-          await CapacitorUpdater.set(data)
-        }
-        catch (error) {
-          console.log(error)
-        }
         }
         console.log("data",JSON.stringify(data),CapacitorUpdater.current())
+        if (!state.isActive && data.version !== "") {
+          // Do the switch when user leave app
+          SplashScreen.show()
+          try {
+            await CapacitorUpdater.set(data)
+          } catch (err) {
+            console.log(err)
+            SplashScreen.hide() // in case the set fail, otherwise the new app will have to hide it
+          }
+        }
     })
       CapacitorUpdater.notifyAppReady();
  return null
